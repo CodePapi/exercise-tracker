695672f5d2a3bece4952b3c7e492b7e0c352af83
CodePapi <49847624+CodePapi@users.noreply.github.com>
2019-12-02 23:27:47 +0100

    mongoose added

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require ("cors")
 const mongoose = require("mongoose")
 
 
-require("dotenv").config();
+require("dotenv").config() ;
 
 const app = express();
 const port = process.env.PORT || 500;
@@ -13,12 +13,18 @@ app.use(express.json());
 
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true})
 
 const connection = mongoose.connection;
 connection.once("open", ()=>{
    console.log("mongoDB data connection has been established")
 })
+
+const exercisesRouters= require("./models/routes/exercise")
+const usersRouters = require("./models/routes/users")
+
+app.use('/exercise', exercisesRouters)
+app.use("/users", usersRouters)
 app.listen(port, ()=>{
     console.log(`port is running the server: ${port}`)
 })
